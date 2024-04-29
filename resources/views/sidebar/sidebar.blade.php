e9b48b4041fad230c334425a0bf5f109ffe11897
akbarMT58 <taufiksakbar@gmail.com>
2024-04-29 22:35:35 +0700

    update final sidebar done all

--- resources/views/sidebar/sidebar.blade.php
+++ resources/views/sidebar/sidebar.blade.php
@@ -25,14 +25,12 @@
 
                     @foreach($data_subchildcategorymenu as $subchild)
 
-                  
-                   
-
+              
                     @if($subchild->sub_childcategorymenu== $modulmenu->id &&  $subchild->categorymenu ==2   )
 
                     <!-- {{$subchild->id}}  -->
 
-                  <li class="{{set_active([$subchild->link_menu])}} submenu">
+                   <li class="{{set_active([$subchild->link_menu])}} submenu">
 
                     <a href="#" class="{{ set_active([$subchild->link_menu]) ? 'noti-dot' : '' }}">
                         <i class="{{$subchild->namaicons}}"></i> <span style="font-size:10px;">{{$subchild->namamenu}} </span> 
@@ -147,10 +145,70 @@
     
                   </li>
 
+                
                
 
 
                 @endif
+
+                @if($subchild->sub_childcategorymenu== $modulmenu->id &&  $subchild->categorymenu ==2 && $subchild->link_menu=='' )
+
+
+
+                <!-- daerah parent dan child or subchildcategorymenu -->
+
+                <li class="{{set_active([$subchild->link_menu])}} submenu">
+
+                  <a  class="{{ set_active([$subchild->link_menu]) ? 'noti-dot' : '' }}">
+                      <i class="{{$subchild->namaicons}}"></i> <span style="font-size:12px;">{{$subchild->namamenu}} </span> 
+                      <span class="menu-arrow"></span>
+                  </a>
+
+                  <ul style="{{ request()->is('/*') ? 'display: block;' : 'display: none;' }}">
+
+                  @foreach($data_subchildcategorymenu as $modul_access_data)
+
+              
+                      @if($modul_access_data->sub_childcategorymenu== $subchild->id &&  $modul_access_data->categorymenu ==3   )
+
+                      <!-- {{$subchild->id}}  -->
+
+                      <li class="subchildmenu" >
+                          
+                          <a class="{{set_active([$modul_access_data->link_menu])}}" id="s_childmenu" style="font-size:10px;" href="{{ url('/') }}{{$modul_access_data->link_menu}}"><span style="font-size:10px;">{{$modul_access_data->namamenu}} </span>  </a>
+    
+                  </li>
+
+
+                      @endif
+
+
+                      @endforeach
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  </ul>
+
+
+
+                <!-- batas  -->
+
+                @endif
+
+
+
 
 
 
